f95180ae8d5f4dd76a89e29235c762e921f86caf
unknown <814258761@qq.com>
2020-12-24 17:31:42 +0800

    Update Solution122.java

--- src/Solution122.java
+++ src/Solution122.java
@@ -2,6 +2,27 @@ public class Solution122 {
     class Solution {
         public int maxProfit(int[] prices) {
 
+            if (prices.length == 1){
+                return 0;
+            }
+
+            int tempPrice = -1;
+            int profit = 0;
+            for (int i = 0; i < prices.length; i++) {
+
+                if (tempPrice == -1){
+                    if (i + 1 < prices.length && prices[i+1] > prices[i]){
+                        tempPrice = prices[i];
+                    }
+                } else {
+                    if (i + 1 == prices.length || prices[i+1] < prices[i] ){
+                        profit += prices[i] - tempPrice;
+                        tempPrice = -1;
+                    }
+                }
+            }
+
+            return profit;
         }
     }
 }
