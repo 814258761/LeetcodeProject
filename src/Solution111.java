a0562ec9598aadaca23b30617c830ae4f1aceda4
unknown <814258761@qq.com>
2020-12-25 18:12:59 +0800

    Create Solution111.java

---
+++ src/Solution111.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.TreeMap;
+
+public class Solution111 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public int minDepth(TreeNode root) {
+            if (root == null){
+                return 0;
+            }
+            
+            if (root.left == null && root.right ==null){
+                return 1;
+            }
+
+            int minDepth = Integer.MAX_VALUE;
+            if (root.left != null){
+                minDepth = Math.min(minDepth, minDepth(root.left));
+            }
+            if (root.right != null){
+                minDepth = Math.min(minDepth, minDepth(root.right));
+            }
+
+            return minDepth + 1;
+        }
+    }
+}
